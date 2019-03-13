19866da641378878e019c6e1cd206d1cc9184a6d
root <root@ip-172-31-31-29.us-east-2.compute.internal>
2019-03-13 04:02:30 +0000

    Command

---
+++ command.sh
@@ -0,0 +1,10 @@
+
+   69  cd opt
+   70  git init
+   71  vi testfile.txt
+   72  git add testfile.txt
+   73  git status
+   74  git commit -m"Demo File"
+   75  git remote add origin https://github.com/gaikwadsant/Dev_OPS.git
+   76  git push -u origin master
+
