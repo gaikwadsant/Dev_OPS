9293c0f21dcb7d5bff7a9ad7256c5f80212e3e30
root <root@ip-172-31-31-29.us-east-2.compute.internal>
2019-03-14 13:42:45 +0000

    Git Command

---
+++ Git_Command.sh
@@ -0,0 +1,18 @@
+mkdir opt Create Empty Dir
+cd opt goes to Opt Dir
+git init : Create empty repositery
+git add fileto place .sh
+git status
+git commit -m""
+git add remote origin
+git push -u origin master
+
+
+
+
+
+next time below four command used for transfer files to Git
+git add fileto place .sh
+git status
+git commit -m""
+git push -u origin master
