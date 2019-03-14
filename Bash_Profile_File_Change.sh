b95d024fd13e83dca11db030bfa4e683e7605aa1
root <root@ip-172-31-31-29.us-east-2.compute.internal>
2019-03-14 13:51:01 +0000

    Bash Changes File

---
+++ Bash_Profile_File_Change.sh
@@ -0,0 +1,12 @@
+There is two changes into File:
+cd ~
+ls -lrt
+files display in that .bashrc and .bash_profile both files display
+
+vi .bashrc file end add some echo "  Data for bashrc"  SAve
+this data display after run bash command
+
+
+vi .bash_profile
+
+It is display after login every time this data display after login
