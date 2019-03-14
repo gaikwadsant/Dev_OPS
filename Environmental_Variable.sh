d81475caea1691b18a653825db65f38b084ba13b
root <root@ip-172-31-31-29.us-east-2.compute.internal>
2019-03-14 13:46:48 +0000

    Env Variables

---
+++ Environmental_Variable.sh
@@ -0,0 +1,17 @@
+There is two types of Variable:
+Local:
+bash create new file
+myname="Santosh" 
+echo $myname
+
+bash 
+then echo $myname data not display because it is local
+
+bash 
+export myname="SANTOSH"
+echo $myname
+bash
+echo $myname
+SANTOSH
+
+
